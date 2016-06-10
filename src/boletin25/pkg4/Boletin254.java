77a5fdf5fd63376029e980a8042db93b45aefe35
Usuario <Usuario@192.168.0.154>
2016-06-10 23:35:16 +0200

    commit

---
+++ src/boletin25/pkg4/Boletin254.java
@@ -0,0 +1,17 @@
+package boletin25.pkg4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin254 {
+
+    
+    public static void main(String[] args) {
+       Frame1 window1= new Frame1();
+       window1.setVisible(true);
+       Frame2 window2= new Frame2();
+       window2.setVisible(true);
+    }
+    
+}
